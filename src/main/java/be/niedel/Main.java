186dfbdee4ffca5603f18fd58161e7ceb1385e18
niels.delestinne <nielsdelestinne@gmail.com>
2024-10-24 16:08:30 +0200

    Remove auto-generated comments

--- src/main/java/be/niedel/Main.java
+++ src/main/java/be/niedel/Main.java
@@ -1,13 +1,9 @@
 package be.niedel;
 
-import com.azure.storage.blob.BlobContainerClient;
-
 import static be.niedel.storageaccount.StorageAccount.createBlobContainerClient;
 import static be.niedel.storageaccount.StorageAccount.createBlobServiceClient;
 import static be.niedel.storageaccount.StorageAccount.uploadBlob;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
